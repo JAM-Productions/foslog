fa3acd430f8945b50252dceb272081ce8bd02fcd
Jordi <55429631+jorbush@users.noreply.github.com>
2025-11-02 18:47:08 +0100

    fmt

--- components/user-menu-skeleton.tsx
+++ components/user-menu-skeleton.tsx
@@ -1,12 +1,12 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 export function UserMenuSkeleton() {
-  return (
-    <div className="flex items-center space-x-4">
-      <Skeleton className="h-8 w-8 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[100px]" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex items-center space-x-4">
+            <Skeleton className="h-8 w-8 rounded-full" />
+            <div className="space-y-2">
+                <Skeleton className="h-4 w-[100px]" />
+            </div>
+        </div>
+    );
 }
