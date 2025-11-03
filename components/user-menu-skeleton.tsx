31d494f4cbec8b7e4414db48b2e44667d1b8edb1
Jordi <55429631+jorbush@users.noreply.github.com>
2025-11-03 20:55:33 +0100

    hide skeleton text on sm

--- components/user-menu-skeleton.tsx
+++ components/user-menu-skeleton.tsx
@@ -4,7 +4,7 @@ export default function UserMenuSkeleton() {
     return (
         <div className="flex items-center space-x-4">
             <Skeleton className="h-8 w-8 rounded-full" />
-            <div className="space-y-2">
+            <div className="hidden space-y-2 sm:block">
                 <Skeleton className="h-4 w-[100px]" />
             </div>
         </div>
