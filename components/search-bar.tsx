0051fa76f49bca4b86ed9c12e9ed3fbb42ff39ef
mriverre8 <rivemark00@gmail.com>
2025-11-15 17:28:18 +0100

    update header behavior

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
     const t = useTranslations('Search');
 
     return (
-        <div className="relative max-w-md flex-1">
+        <div className="relative flex-1">
             <Search className="text-muted-foreground absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 transform" />
             <input
                 type="text"
