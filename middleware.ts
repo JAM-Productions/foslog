14ab81c7229a0b256ca3d517d56f8947f98fd5ba
mriverre8 <rivemark00@gmail.com>
2026-01-24 21:33:31 +0100

    matcher for logs

--- middleware.ts
+++ middleware.ts
@@ -84,8 +84,6 @@ export const config = {
     // Match all pathnames except for
     // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
-    matcher: [
-        '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-    ],
+    matcher:
+        '/((?!api|_next/static|_next/image|favicon.*|sitemap.xml|robots.txt|locales/*|manifest.json).*)',
 };
