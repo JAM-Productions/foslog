c8ccb1476824dae01443fa9a2c44be3c918275d5
mriverre8 <rivemark00@gmail.com>
2026-02-16 21:26:21 +0100

    jam logo in footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -2,6 +2,7 @@
 import pkg from '../../package.json';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 
 const Footer = () => {
     const t = useTranslations('Footer');
@@ -41,6 +42,33 @@ const Footer = () => {
                 >
                     v{pkg.version}
                 </a>
+            </div>
+            <div className="container mx-auto mt-4 flex flex-col items-center justify-center gap-4 text-sm md:flex-row md:gap-8">
+                <div className="flex items-center">
+                    <a
+                        href="https://github.com/JAM-Productions"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Visit JAM Productions GitHub profile (opens in new tab)"
+                    >
+                        <Image
+                            src="/jam-productions-logo.png"
+                            alt=""
+                            width={23}
+                            height={23}
+                            className="inline-block rounded-sm"
+                        />
+                    </a>
+                    <a
+                        href="https://github.com/JAM-Productions"
+                        className="ml-2 hover:underline"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Visit JAM Productions GitHub profile (opens in new tab)"
+                    >
+                        JAM Productions
+                    </a>
+                </div>
             </div>
         </footer>
     );
